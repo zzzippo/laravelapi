9524ac71ffbd3a2822983969abbd86988246238b
slyvanasGG <77849093@qq.com>
2016-01-02 16:36:09 +0800

    *baseRe

--- app/Repositories/BaseRepository.php
+++ app/Repositories/BaseRepository.php
@@ -5,30 +5,5 @@ namespace App\Repositories;
 trait BaseRepository
 {
 
-    /**
-     * Destroy a model.
-     *
-     * @param int $id
-     */
-    public function destroy($id)
-    {
-        $this->getById($id)->delete();
-    }
 
-    /**
-     * Get Model by id.
-     *
-     * @param int $id
-     *
-     * @return App\Models\Model
-     */
-    public function getById($id)
-    {
-        return $this->model->find($id);
-    }
-
-    public function getAll()
-    {
-        return $this->model->all();
-    }
 }
--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -112,4 +112,31 @@ class UserRepository
         return $user->save();
     }
 
+    /**
+     * Destroy a model.
+     *
+     * @param int $id
+     */
+    public function destroy($id)
+    {
+        $this->getById($id)->delete();
+    }
+
+    /**
+     * Get Model by id.
+     *
+     * @param int $id
+     *
+     * @return App\Models\Model
+     */
+    public function getById($id)
+    {
+        return $this->model->find($id);
+    }
+
+    public function getAll()
+    {
+        return $this->model->all();
+    }
+
 }
